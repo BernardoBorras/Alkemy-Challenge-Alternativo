606553b2d7f1ee71ab042e41759315564195f169
Bernardo Borras <bernardo.borras@hotmail.com>
2022-07-04 23:56:37 -0300

    Primer Commit Enlazando el repositorio local descargado de Spring initializer. Con el repositorio remoto.

---
+++ src/main/java/com/alkemy/icons/icons/IconsApplication.java
@@ -0,0 +1,13 @@
+package com.alkemy.icons.icons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IconsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IconsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alkemy/icons/icons/IconsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alkemy.icons.icons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IconsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
